4d2251874fe29721bb366fcb840ed273e6e1de13
vikram <maticsystem1@gmail.com>
2023-05-06 09:24:08 +0530

    BMI

---
+++ BMI/src/Components/Bmi.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Bmi() {
+  return (
+ <>
+<div className="container">
+  <div className="form_details">BMI CALCULATOR</div>
+  <form id="myForm">
+    <div className="h">
+      <label htmlFor="height">Height : </label>
+      <input placeholder="Enter your Height" type="number" className="form-control" id="height" />
+    </div>
+    <br />
+    <div className="w">
+      <label htmlFor="weight">Weight : </label>
+      <input placeholder="Enter your weight" type="number" className="form-control" id="height" />
+    </div>
+    <br />
+    <div className="radio-container">
+      <div className="radio-wrapper">
+        <label className="radio-button">
+          <input type="radio" name="radio-group" id="option1" />
+          <span className="radio-checkmark" />
+          <span className="radio-label">Male</span>
+        </label>
+      </div>
+      <div className="radio-wrapper">
+        <label className="radio-button">
+          <input type="radio" name="radio-group" id="option2" />
+          <span className="radio-checkmark" />
+          <span className="radio-label">Female</span>
+        </label>
+      </div>
+      <button>Calculate</button>
+    </div></form>
+</div>
+
+ 
+ 
+ </>
+  )
+}
+
+export default Bmi
