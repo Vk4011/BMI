5edd6de5e05225d0b94e525b1f13c8b5e27df20e
vikram <maticsystem1@gmail.com>
2023-05-07 19:48:56 +0530

    Result

--- BMI/src/Components/Bmi.jsx
+++ BMI/src/Components/Bmi.jsx
@@ -11,7 +11,6 @@ function Bmi() {
       <label htmlFor="height">Height : </label>
       <input placeholder="Enter your Height" type="number" className="form-control" id="height" />
     </div>
-    <br />
     <div className="w">
       <label htmlFor="weight">Weight : </label>
       <input placeholder="Enter your weight" type="number" className="form-control" id="height" />
@@ -33,7 +32,10 @@ function Bmi() {
         </label>
       </div>
     </div>
-      <button>Calculate</button>
+    <p id="output">Result : </p>
+
+      <button type="submit" onclick="calculateBMI()">Calculate</button>
+
     </form>
 </div>
 
@@ -44,3 +46,33 @@ function Bmi() {
 }
 
 export default Bmi
+
+function calculateBMI( ) {
+  let height = parseFloat(document.getElementById( 'height' ). value);
+  let weight = parseFloat(document.getElementById( 'weight' ).value);
+  let showOutput = document.getElementById( 'output');
+  let classification;
+  let bmiRange = weight/ (height / 100) ** 2 ;
+  bmiRange= bmiRange.toFixed(2);
+  
+  if(bmiRange < 16){
+    classification = 'Severe Thinness';
+  }else if(bmiRange > 16 && bmiRange < 17){
+    classification = 'Moderate Thinness';
+  }else if(bmiRange > 17 && bmiRange < 18.5){
+    classification = 'Mild Thinness';
+  }else if(bmiRange > 18.5 && bmiRange < 25){
+    classification = 'Normal';
+  }else if(bmiRange > 25 && bmiRange < 30){
+    classification = 'Over Weight';
+  }else if(bmiRange > 30 && bmiRange < 34){
+    classification = 'Obese class I';
+  }else if(bmiRange > 35 && bmiRange < 40){
+    classification = 'Obese class II';
+  }else if (bmiRange > 40){
+    classification = 'Obese class III';
+  }
+  showOutput.innerHTML = `BMI Range: ${bmiRange} | ${classification}`;
+  
+  
+}
