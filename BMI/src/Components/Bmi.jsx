120d593908f781f5e4ffb5bd73c6075838d9f680
vikram <maticsystem1@gmail.com>
2023-05-07 20:58:03 +0530

    Frontend

--- BMI/src/Components/Bmi.jsx
+++ BMI/src/Components/Bmi.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Bmi.css'
 
 function Bmi() {
+
+  
   return (
  <>
 <div className="container">
@@ -48,31 +50,13 @@ function Bmi() {
 export default Bmi
 
 function calculateBMI( ) {
-  let height = parseFloat(document.getElementById( 'height' ). value);
-  let weight = parseFloat(document.getElementById( 'weight' ).value);
-  let showOutput = document.getElementById( 'output');
-  let classification;
-  let bmiRange = weight/ (height / 100) ** 2 ;
-  bmiRange= bmiRange.toFixed(2);
+  let height = parseFloat(document.getElementById("height"). value);
+  let weight = parseFloat(document.getElementById("weight").value);
+  let showOutput = document.getElementById('output');
+  let bmi = weight/ (height / 100) ** 2 ;
   
-  if(bmiRange < 16){
-    classification = 'Severe Thinness';
-  }else if(bmiRange > 16 && bmiRange < 17){
-    classification = 'Moderate Thinness';
-  }else if(bmiRange > 17 && bmiRange < 18.5){
-    classification = 'Mild Thinness';
-  }else if(bmiRange > 18.5 && bmiRange < 25){
-    classification = 'Normal';
-  }else if(bmiRange > 25 && bmiRange < 30){
-    classification = 'Over Weight';
-  }else if(bmiRange > 30 && bmiRange < 34){
-    classification = 'Obese class I';
-  }else if(bmiRange > 35 && bmiRange < 40){
-    classification = 'Obese class II';
-  }else if (bmiRange > 40){
-    classification = 'Obese class III';
-  }
-  showOutput.innerHTML = `BMI Range: ${bmiRange} | ${classification}`;
+  
+  showOutput.innerHTML = `Result : ${bmi}`;
   
   
 }
